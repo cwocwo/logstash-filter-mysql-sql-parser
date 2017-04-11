df93d2fa898d4711fd585a6971ae0d734395ad0a
蔡卫卫 <caiweiwei@inspur.com>
2017-04-11 16:49:02 +0800

    Init plugin

---
+++ DEVELOPER.md
@@ -0,0 +1,2 @@
+# logstash-filter-mysql-sql-parser
+Example filter plugin. This should help bootstrap your effort to write your own filter plugin!
---
+++ spec/filters/mysql-sql-parser_spec.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+require_relative '../spec_helper'
+require "logstash/filters/mysql-sql-parser"
+
+describe LogStash::Filters::MysqlSqlParser do
+  describe "Set to Hello World" do
+    let(:config) do <<-CONFIG
+      filter {
+        mysql-sql-parser {
+          message => "Hello World"
+        }
+      }
+    CONFIG
+    end
+
+    sample("message" => "some text") do
+      expect(subject).to include("message")
+      expect(subject.get('message')).to eq('Hello World')
+    end
+  end
+end
